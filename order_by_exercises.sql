d47ad30f7865357f2d3a9f2037ae550732958659
tcolejimenez <taylor.cole.jimenez@gmail.com>
2018-10-05 15:53:18 -0500

    Completed order by exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,12 @@
+USE employees;
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name DESC, first_name DESC;
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
+
+
+
+SELECT * FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-24'
+ORDER BY birth_date, hire_date;
