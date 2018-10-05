aa2b57c9b606e76d90af6be553d831f3aa064eb8
tcolejimenez <taylor.cole.jimenez@gmail.com>
2018-10-05 15:58:59 -0500

    Completed all clauses exercises

---
+++ limit_exercises.sql
@@ -0,0 +1,15 @@
+SELECT DISTINCT last_name FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+SELECT * FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-24'
+ORDER BY birth_date, hire_date
+LIMIT 5;
+
+SELECT * FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-24'
+ORDER BY birth_date, hire_date
+LIMIT 5 OFFSET 50; 
